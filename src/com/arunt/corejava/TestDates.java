c71baa4a5f9e461fc7501305698ff998b56dbabf
Arun Tummalapalli <swarun99@yahoo.com>
2018-02-03 18:03:37 -0500

    Added local dates

---
+++ src/com/arunt/corejava/TestDates.java
@@ -0,0 +1,15 @@
+package com.arunt.corejava;
+
+import java.time.LocalDate;
+
+public class TestDates {
+
+
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+        System.out.println("today " + today);
+    }
+
+
+}
