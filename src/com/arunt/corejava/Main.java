23bf5110f14ff4c3061bd547dc0f380afda02208
Arun Tummalapalli <swarun99@yahoo.com>
2018-02-03 17:23:43 -0500

    Initial Commit

---
+++ src/com/arunt/corejava/Main.java
@@ -0,0 +1,22 @@
+package com.arunt.corejava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Hello my time");
+
+        Thread t = new Thread( new TestThread(), "t1");
+        Thread t2 = new Thread( new TestThread(), "t2");
+        Thread t21 = new Thread( new TestThread(), "t3");
+        Thread t3 = new Thread( new TestThread(), "t4");
+        Thread t4 = new Thread( new TestThread(), "t5");
+
+        t.start();
+        t2.start();
+        t21.start();
+        t3.start();
+        t4.start();
+    }
+}
